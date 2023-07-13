ebac1d8c715eec03860830148f8b76e3a7726759
Olusanmi Hundogan <o.hundogan@gmail.com>
2023-07-13 16:50:02 +0200

    Impl: Adaboost basic scope estimator

--- experiments/experiment_estimators.py
+++ experiments/experiment_estimators.py
@@ -1,17 +1,15 @@
-from datasources.core import get_small_datamanager_configuration
+from datasources.core import get_default_datamanager_configuration, get_small_datamanager_configuration
+from feature_reducers.core import PCAFeatureReducer
 from pipeline.core import DefaultPipeline, FSExperimentPipeline
-from dataset_loader.csv_loader import FSExperimentDataLoader, DefaultDataManager
-from base import OxariDataManager, OxariSavingManager, LocalMetaModelSaver, LocalLARModelSaver, LocalDataSaver
+from base import OxariDataManager
 from preprocessors import BaselinePreprocessor, IIDPreprocessor
 from postprocessors import ScopeImputerPostprocessor
 # from imputers.revenue_bucket import RevenueBucketImputer
 from imputers import BaselineImputer, RevenueQuantileBucketImputer
-from feature_reducers import DummyFeatureReducer, PCAFeatureSelector, DropFeatureReducer, IsomapDimensionalityReduction, MDSDimensionalitySelector, FeatureAgglomeration, GaussRandProjection, SparseRandProjection, Factor_Analysis, Latent_Dirichlet_Allocation
 from scope_estimators import PredictMedianEstimator, GaussianProcessEstimator, MiniModelArmyEstimator, DummyEstimator, PredictMeanEstimator, BaselineEstimator
 from scope_estimators import SupportVectorEstimator, XGBEstimator, SGDEstimator, PLSEstimator, GaussianProcessEstimator, DummyEstimator, KNNEstimator, RNEstimator, LinearSVREstimator, BayesianRegressionEstimator, MLPEstimator
 from base import BaselineConfidenceEstimator
 from base.helper import LogTargetScaler
-from scope_estimators import MiniModelPartyEstimator
 # import base
 # from base import helper
 from base import OxariMetaModel
@@ -20,15 +18,17 @@ import pandas as pd
 # from dataset_loader.csv_loader import CSVScopeLoader, CSVFinancialLoader, CSVCategoricalLoader
 import time
 
+from scope_estimators.adaboost import AdaboostEstimator
+
 if __name__ == "__main__":
 
     all_results = []
+    dataset = get_default_datamanager_configuration().run()  # run() calls _transform()
     # loads the data just like CSVDataLoader, but a selection of the data
-    for i in range(1):
-        # configurations = [MiniModelArmyEstimator(), MiniModelPartyEstimator(), BaselineEstimator(), PredictMedianEstimator()]
-        configurations = [ MLPEstimator()]
+    for i in range(10):
+        # configurations = [MiniModelArmyEstimator(), BaselineEstimator(), PredictMedianEstimator()]
+        configurations = [AdaboostEstimator(), MLPEstimator(), BaselineEstimator(), PredictMedianEstimator()]
         # configurations = [SupportVectorEstimator(), XGBEstimator(), SGDEstimator(), DummyEstimator()]
-        dataset = get_small_datamanager_configuration().run()  # run() calls _transform()
         bag = dataset.get_split_data(OxariDataManager.ORIGINAL)
         SPLIT_1 = bag.scope_1
         SPLIT_2 = bag.scope_2
@@ -41,34 +41,35 @@ if __name__ == "__main__":
 
             ppl1 = DefaultPipeline(
                 preprocessor=IIDPreprocessor(),
-                feature_reducer=PCAFeatureSelector(),
+                feature_reducer=PCAFeatureReducer(),
                 imputer=RevenueQuantileBucketImputer(),
                 scope_estimator=estimator,
                 ci_estimator=BaselineConfidenceEstimator(),
                 scope_transformer=LogTargetScaler(),
             ).optimise(*SPLIT_1.train).fit(*SPLIT_1.train).evaluate(*SPLIT_1.rem, *SPLIT_1.val).fit_confidence(*SPLIT_1.train)
+            all_results.append({"time": time.time() - start, "scope": 1, **ppl1.evaluation_results})
             ppl2 = DefaultPipeline(
                 preprocessor=IIDPreprocessor(),
-                feature_reducer=PCAFeatureSelector(),
+                feature_reducer=PCAFeatureReducer(),
                 imputer=RevenueQuantileBucketImputer(),
                 scope_estimator=estimator,
                 ci_estimator=BaselineConfidenceEstimator(),
                 scope_transformer=LogTargetScaler(),
             ).optimise(*SPLIT_2.train).fit(*SPLIT_2.train).evaluate(*SPLIT_2.rem, *SPLIT_2.val).fit_confidence(*SPLIT_2.train)
+            all_results.append({"time": time.time() - start, "scope": 2, **ppl2.evaluation_results})
             ppl3 = DefaultPipeline(
                 preprocessor=IIDPreprocessor(),
-                feature_reducer=PCAFeatureSelector(),
+                feature_reducer=PCAFeatureReducer(),
                 imputer=RevenueQuantileBucketImputer(),
                 scope_estimator=estimator,
                 ci_estimator=BaselineConfidenceEstimator(),
                 scope_transformer=LogTargetScaler(),
             ).optimise(*SPLIT_3.train).fit(*SPLIT_3.train).evaluate(*SPLIT_3.rem, *SPLIT_3.val).fit_confidence(*SPLIT_3.train)
-
-            all_results.append({"time": time.time() - start, "scope": 1, **ppl1.evaluation_results})
-            all_results.append({"time": time.time() - start, "scope": 2, **ppl2.evaluation_results})
             all_results.append({"time": time.time() - start, "scope": 3, **ppl3.evaluation_results})
-        ### EVALUATION RESULTS ###
+            ### EVALUATION RESULTS ###
             concatenated = pd.json_normalize(all_results)[[
                 "time", "scope", "imputer", "preprocessor", "feature_selector", "scope_estimator", "test.evaluator", "test.sMAPE", "test.R2", "test.MAE", "test.RMSE", "test.MAPE"
             ]]
-            concatenated.to_csv('local/eval_results/experiment_estimators.csv')
+
+            fname = __loader__.name.split(".")[-1]
+            concatenated.to_csv(f'local/eval_results/{fname}.csv')

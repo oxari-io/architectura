51c20adecc203916449a0cf99733c9d9d5df4b4f
Olusanmi Hundogan <o.hundogan@gmail.com>
2023-07-19 19:54:33 +0200

    Added data features analysis for defeating multicolinearity

--- notebooks/analyze_data_features.py
+++ notebooks/analyze_data_features.py
@@ -1,8 +1,13 @@
 # %%
 import sys
+from base.dataset_loader import CategoricalLoader, CompanyDataFilter, FinancialLoader, ScopeLoader
+from datasources.loaders import RegionLoader
+
+from datasources.local import LocalDatasource
 
 sys.path.append("..")
 import pathlib
+from IPython.display import display
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,14 +16,20 @@ import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from base import OxariDataManager
-from datasources.core import DefaultDataManager
+from datasources.core import DefaultDataManager, PreviousScopeFeaturesDataManager
 from datasources.online import S3Datasource
-
+from pathlib import Path
 sns.set_palette('viridis')
 
+PARENT_PATH = Path('..').absolute().resolve().as_posix()
+PARENT_PATH
 # %%
-dataset = DefaultDataManager(S3Datasource(path='model-input-data/scopes_auto.csv'), S3Datasource(path='model-input-data/financials_auto.csv'),
-                             S3Datasource(path='model-input-data/categoricals_auto.csv')).run()
+dataset = PreviousScopeFeaturesDataManager(
+    FinancialLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/financials_auto.csv")),
+    ScopeLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/scopes_auto.csv")),
+    CategoricalLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/categoricals_auto.csv")),
+    RegionLoader(),
+).set_filter(CompanyDataFilter(frac=1)).run()
 DATA = dataset.get_data_by_name(OxariDataManager.ORIGINAL)
 DATA
 # %%
@@ -56,6 +67,7 @@ for ax, feature in zip(faxes, numerical_features.columns):
     # sns.scatterplot(tmp_df, x=feature, y=target, ax=ax)
 
     sns.histplot(x=np.arcsinh(tmp_df[feature]), ax=ax, bins=num_bins)
+    # sns.histplot(x=tmp_df[feature], ax=ax, bins=num_bins)
 fig.tight_layout()
 plt.show()
 # %%
@@ -78,5 +90,99 @@ for axs, feature in zip(axes, numerical_features.columns):
 
 fig.tight_layout()
 plt.show()
+
+# %%
+thresh=0.5
+
+# %%
+correlations_original = numerical_features.corr()
+correlations_original
+# %%
+plt.figure(figsize=(25,20))
+sns.heatmap(correlations_original.abs(), vmin=-1, vmax=1, cmap='bwr')
+
+# %%
+correlations = correlations_original.copy()
+flag = True
+while flag:
+    highest_corrs = list(np.sum(correlations.abs() > thresh).sort_values().items())[-1]
+    if highest_corrs[1] < 2:
+        break
+    print(f"Going to remove {highest_corrs}")
+    correlations = correlations.drop(highest_corrs[0], axis=1).drop(highest_corrs[0], axis=0)
+    # display(correlations)
+# %%    
+plt.figure(figsize=(25,20))
+sns.heatmap(correlations.abs(), vmin=-1, vmax=1,cmap='bwr')
+# %%
+correlations_strict = correlations_original.copy()
+l_highest_corrs = list(np.sum(correlations_strict.abs() > thresh).sort_values(ascending=0).items())
+reversed(l_highest_corrs)
+for key, val in l_highest_corrs:
+    if val > 1: 
+        print(f"Going to remove {(key, val)}")
+        correlations_strict = correlations_strict.drop(key, axis=1).drop(key, axis=0) 
+# %%
+plt.figure(figsize=(25,20))
+sns.heatmap(correlations_strict.abs(), vmin=-1, vmax=1,cmap='bwr')
+
+# %%
+# https://www.projectpro.io/recipes/drop-out-highly-correlated-features-in-python
+
+from sklearn.feature_selection import SelectKBest
+
+from sklearn.feature_selection import f_regression
+
+# define number of features to keep
+
+k = 10
+
+# perform feature selection
+y = DATA['tg_numc_scope_1']
+selector = SelectKBest(f_regression, k=k).fit(numerical_features.fillna(1), y.fillna(1))
+X_new = selector.transform(numerical_features.fillna(1))
+# get feature names of selected features
+
+selected_features = numerical_features.columns[selector.get_support()]
+
+# print selected features
+
+print(selected_features)
+# %%
+plt.figure(figsize=(25,20))
+sns.heatmap(numerical_features[selected_features].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+
+# %%
+# %%
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from tqdm import tqdm
+# calculate VIF for each feature
+
+vif = pd.DataFrame()
+
+vif["VIF Factor"] = [variance_inflation_factor(numerical_features.fillna(0).values, i) for i in tqdm(range(numerical_features.shape[1]))]
+
+vif["features"] = numerical_features.columns
+
+# print VIF values
+
+print(vif)
+# %%
+vif[vif["VIF Factor"] < 10].features.tolist()
+# %%
+plt.figure(figsize=(25,20))
+sns.heatmap(numerical_features[vif[vif["VIF Factor"] < 10].features.tolist()].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+# %%
+from sklearn.feature_selection import RFECV
+from xgboost import XGBRegressor
+
+estimator = XGBRegressor()
+selector = RFECV(estimator, step=1, cv=5, verbose=True)
+selector = selector.fit(numerical_features[~y.isna()], y[~y.isna()])
+
+# %%
+
+plt.figure(figsize=(25,20))
+sns.heatmap(numerical_features[selector.support_].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
 
 # %%

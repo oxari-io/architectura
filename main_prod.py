5586916e9e726252b886fc4115ee84e111d4c907
Olusanmi Hundogan <o.hundogan@gmail.com>
2023-07-28 15:01:29 +0200

    Impl: Script modification to generate two models

--- main_prod.py
+++ main_prod.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import PowerTransformer
 
 from base import (OxariDataManager, OxariMetaModel, helper)
 from base.confidence_intervall_estimator import BaselineConfidenceEstimator
+from base.constants import FEATURE_SET_VIF_UNDER_10
 from base.dataset_loader import CategoricalLoader, FinancialLoader, ScopeLoader
 from base.helper import DummyTargetScaler, LogTargetScaler
 from base.run_utils import compute_jump_rates, compute_lar, impute_missing_years, impute_scopes
@@ -15,6 +16,7 @@ from datasources.core import PreviousScopeFeaturesDataManager, get_default_datam
 from datasources.loaders import RegionLoader
 from datastores.saver import CSVSaver, LocalDestination, MongoDestination, MongoSaver, OxariSavingManager, PickleSaver, S3Destination
 from feature_reducers import DummyFeatureReducer
+from feature_reducers.core import SelectionFeatureReducer
 from imputers import RevenueQuantileBucketImputer
 from imputers.iterative import OldOxariImputer
 from pipeline.core import DefaultPipeline
@@ -50,15 +52,7 @@ STAGE = "p_"
 # TODO: Convert some of the main_*.py scripts to experiments.
 
 
-
-
-
-
-if __name__ == "__main__":
-    today = time.strftime(DATE_FORMAT)
-    now = time.strftime('T%Y%m%d%H%M')
-
-    dataset = get_default_datamanager_configuration().run()
+def train_model_for_imputation(N_TRIALS, N_STARTUP_TRIALS, dataset):
     DATA = dataset.get_data_by_name(OxariDataManager.ORIGINAL)
     bag = dataset.get_split_data(OxariDataManager.ORIGINAL)
     SPLIT_1 = bag.scope_1
@@ -95,29 +89,81 @@ if __name__ == "__main__":
     model.add_pipeline(scope=1, pipeline=dp1)
     model.add_pipeline(scope=2, pipeline=dp2)
     model.add_pipeline(scope=3, pipeline=dp3)
+    return model
 
-    print("Parameter Configuration")
-    print(dp1.get_config(deep=True))
-    print(dp2.get_config(deep=True))
-    print(dp3.get_config(deep=True))
+def train_model_for_live_prediction(N_TRIALS, N_STARTUP_TRIALS, dataset):
+    DATA = dataset.get_data_by_name(OxariDataManager.ORIGINAL)
+    bag = dataset.get_split_data(OxariDataManager.ORIGINAL)
+    SPLIT_1 = bag.scope_1
+    SPLIT_2 = bag.scope_2
+    SPLIT_3 = bag.scope_3
+
+    # Test what happens if not all the optimise functions are called.
+    dp1 = DefaultPipeline(
+        preprocessor=IIDPreprocessor(fin_transformer=PowerTransformer()),
+        feature_reducer=SelectionFeatureReducer(FEATURE_SET_VIF_UNDER_10),
+        imputer=RevenueQuantileBucketImputer(10),
+        scope_estimator=MiniModelArmyEstimator(10, n_trials=N_TRIALS, n_startup_trials=N_STARTUP_TRIALS),
+        ci_estimator=BaselineConfidenceEstimator(),
+        scope_transformer=LogTargetScaler(),
+    ).optimise(*SPLIT_1.train).fit(*SPLIT_1.train).evaluate(*SPLIT_1.rem, *SPLIT_1.val).fit_confidence(*SPLIT_1.train)
+    dp2 = DefaultPipeline(
+        preprocessor=IIDPreprocessor(fin_transformer=PowerTransformer()),
+        feature_reducer=SelectionFeatureReducer(FEATURE_SET_VIF_UNDER_10),
+        imputer=RevenueQuantileBucketImputer(10),
+        scope_estimator=MiniModelArmyEstimator(10, n_trials=N_TRIALS, n_startup_trials=N_STARTUP_TRIALS),
+        ci_estimator=BaselineConfidenceEstimator(),
+        scope_transformer=LogTargetScaler(),
+    ).optimise(*SPLIT_2.train).fit(*SPLIT_2.train).evaluate(*SPLIT_2.rem, *SPLIT_2.val).fit_confidence(*SPLIT_2.train)
+    dp3 = DefaultPipeline(
+        preprocessor=IIDPreprocessor(fin_transformer=PowerTransformer()),
+        feature_reducer=SelectionFeatureReducer(FEATURE_SET_VIF_UNDER_10),
+        imputer=RevenueQuantileBucketImputer(10),
+        scope_estimator=MiniModelArmyEstimator(10, n_trials=N_TRIALS, n_startup_trials=N_STARTUP_TRIALS),
+        ci_estimator=BaselineConfidenceEstimator(),
+        scope_transformer=LogTargetScaler(),
+    ).optimise(*SPLIT_3.train).fit(*SPLIT_3.train).evaluate(*SPLIT_3.rem, *SPLIT_3.val).fit_confidence(*SPLIT_3.train)
+
+    model = OxariMetaModel()
+    model.add_pipeline(scope=1, pipeline=dp1)
+    model.add_pipeline(scope=2, pipeline=dp2)
+    model.add_pipeline(scope=3, pipeline=dp3)
+    return model
+
+
+if __name__ == "__main__":
+    today = time.strftime(DATE_FORMAT)
+    now = time.strftime('T%Y%m%d%H%M')
+
+    dataset = get_default_datamanager_configuration().run()
+    # Scope Imputation model
+    model_si = train_model_for_imputation(N_TRIALS, N_STARTUP_TRIALS, dataset) 
+    # Live Prediciton model
+    model_lp = train_model_for_live_prediction(N_TRIALS, N_STARTUP_TRIALS, dataset)
+
+    # TODO: Convert to a pytest
+    # print("Parameter Configuration")
+    # print(model_si.get_pipeline(1).get_config(deep=True))
+    # print(model_si.get_pipeline(2).get_config(deep=True))
+    # print(model_si.get_pipeline(3).get_config(deep=True))
 
     ### EVALUATION RESULTS ###
     print("Eval results")
-    eval_results = pd.json_normalize(model.collect_eval_results())
-    print(eval_results.T)
-    eval_results.T.to_csv(f'local/prod_runs/model_pipelines_{now}.csv')
+    eval_results_1 = pd.json_normalize(model_si.collect_eval_results())
+    eval_results_2 = pd.json_normalize(model_lp.collect_eval_results())
+    pd.concat([eval_results_1, eval_results_2]).T.to_csv(f'local/prod_runs/model_pipelines_{now}.csv')
+
+    # TODO: Convert to a pytest 
     # print("Predict with Pipeline")
     # print(dp1.predict(X))
-    print("Predict with Model only SCOPE1")
-    print(model.predict(SPLIT_1.val.X, scope=1))
 
+    # TODO: Convert to a pytest
+    # print("Predict with Model only SCOPE1")
+    # bag = dataset.get_split_data(OxariDataManager.ORIGINAL)
+    # SPLIT_1 = bag.scope_1
+    # print(model.predict(SPLIT_1.val.X, scope=1))
 
-
-
-
-
-
-
+    # TODO: Turn to a script
     # print("Explain Effects of features")
     # explainer0 = ShapExplainer(model.get_pipeline(1), sample_size=100).fit(*SPLIT_1.train).explain(*SPLIT_1.val)
     # fig, ax = explainer0.visualize()
@@ -131,15 +177,19 @@ if __name__ == "__main__":
     #     fig, ax = expl.plot_importances()
     #     fig.savefig(f'local/eval_results/importance_explainer{intervall_group+1}.png')
 
+    # TODO: Convert to a pytest
     # print("\n", "Predict ALL with Model")
     # print(model.predict(SPLIT_1.val.X))
 
+    # TODO: Convert to a pytest
     # print("\n", "Predict ALL on Mock data")
     # print(model.predict(helper.mock_data()))
 
+    # TODO: Convert to a pytest
     # print("\n", "Compute Confidences")
     # print(model.predict(SPLIT_1.val.X, return_ci=True))
 
+    # TODO: Convert to an analysis script
     # print("\n", "DIRECT COMPARISON")
     # X_new = model.predict(SPLIT_1.test.X, scope=1, return_ci=True)
     # X_new["true_scope"] = SPLIT_1.test.y.values
@@ -150,18 +200,15 @@ if __name__ == "__main__":
     # print(X_new)
 
     # tmp_pipeline = model.get_pipeline(1)
-
     # tmp_pipeline.feature_selector.visualize(tmp_pipeline._preprocess(X))
     ## SAVE OBJECTS ###
 
     all_meta_models = [
-        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model").set_object(model).set_datatarget(LocalDestination(path="model-data/output")),
-        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model").set_object(model).set_datatarget(S3Destination(path="model-data/output")),
+        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model_scope_imputation").set_object(model_si).set_datatarget(LocalDestination(path="model-data/output")),
+        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model_scope_imputation").set_object(model_si).set_datatarget(S3Destination(path="model-data/output")),
+        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model").set_object(model_lp).set_datatarget(LocalDestination(path="model-data/output")),
+        PickleSaver().set_time(time.strftime(DATE_FORMAT)).set_extension(".pkl").set_name("p_model").set_object(model_lp).set_datatarget(S3Destination(path="model-data/output")),
     ]
 
-
-
-    SavingManager = OxariSavingManager(
-        *all_meta_models,
-    )
+    SavingManager = OxariSavingManager(*all_meta_models, )
     SavingManager.run()
---
+++ notebooks/analyze_feature_set_initial_discovery.py
@@ -0,0 +1,161 @@
+# %%
+import sys
+
+sys.path.append("..")
+import pathlib
+from IPython.display import display
+
+from datasources.loaders import RegionLoader
+from datasources.local import LocalDatasource
+from base.dataset_loader import CategoricalLoader, CompanyDataFilter, FinancialLoader, ScopeLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from base import OxariDataManager
+from datasources.core import DefaultDataManager, PreviousScopeFeaturesDataManager
+from datasources.online import S3Datasource
+from pathlib import Path
+
+sns.set_palette('viridis')
+
+PARENT_PATH = Path('..').absolute().resolve().as_posix()
+PARENT_PATH
+# %%
+dataset = PreviousScopeFeaturesDataManager(
+    FinancialLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/financials_auto.csv")),
+    ScopeLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/scopes_auto.csv")),
+    CategoricalLoader(datasource=LocalDatasource(path=PARENT_PATH + "/model-data/input/categoricals_auto.csv")),
+    RegionLoader(),
+).set_filter(CompanyDataFilter(frac=1)).run()
+DATA = dataset.get_data_by_name(OxariDataManager.ORIGINAL)
+DATA
+# %%
+df_scopes = DATA
+df_scopes["grp_scope_1"] = None
+df_scopes["log_scope_1"] = None
+df_scopes.loc[df_scopes["tg_numc_scope_1"].isna(), ["grp_scope_1"]] = "Not reported"
+df_scopes.loc[df_scopes["tg_numc_scope_1"] == 0, ["grp_scope_1"]] = "Zero Emissions"
+df_scopes.loc[df_scopes["tg_numc_scope_1"] < 0, ["grp_scope_1"]] = "Impossible"
+df_scopes.loc[df_scopes["tg_numc_scope_1"].between(0, 1, inclusive='right'), ["grp_scope_1"]] = "Weird"
+df_scopes.loc[df_scopes["tg_numc_scope_1"] > 1, ["grp_scope_1"]] = "Emittor"
+df_scopes["log_scope_1"] = np.log(df_scopes["tg_numc_scope_1"])
+indices = df_scopes["tg_numc_scope_1"] > 0
+df_scopes
+
+# %%
+numerical_features = df_scopes.filter(regex="^ft_numc", axis=1)
+
+# %%
+thresh = 0.5
+from sklearn.impute import KNNImputer, SimpleImputer
+
+# %%
+correlations_original = numerical_features.corr()
+correlations_original
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(correlations_original.abs(), vmin=-1, vmax=1, cmap='bwr')
+
+# %%
+correlations = correlations_original.copy()
+flag = True
+while flag:
+    highest_corrs = list(np.sum(correlations.abs() > thresh).sort_values().items())[-1]
+    if highest_corrs[1] < 2:
+        break
+    # print(f"Going to remove {highest_corrs}")
+    correlations = correlations.drop(highest_corrs[0], axis=1).drop(highest_corrs[0], axis=0)
+    # display(correlations)
+print('Iterative elimination\n')
+print(f"features_iterative_corr_elimination = {list(correlations.columns)}")      
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(correlations.abs(), vmin=-1, vmax=1, cmap='bwr')
+# %%
+correlations_strict = correlations_original.copy()
+l_highest_corrs = list(np.sum(correlations_strict.abs() > thresh).sort_values(ascending=0).items())
+reversed(l_highest_corrs)
+for key, val in l_highest_corrs:
+    if val > 1:
+        # print(f"Going to remove {(key, val)}")
+        correlations_strict = correlations_strict.drop(key, axis=1).drop(key, axis=0)
+
+print('Strict elimination\n')
+print(f"features_strict_corr_elimination = {list(correlations_strict.columns)}")  
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(correlations_strict.abs(), vmin=-1, vmax=1, cmap='bwr')
+# %%
+numerical_features = pd.DataFrame(SimpleImputer(strategy='median').fit_transform(numerical_features), columns=numerical_features.columns, index=numerical_features.index)
+numerical_features
+# %%
+# https://www.projectpro.io/recipes/drop-out-highly-correlated-features-in-python
+
+from sklearn.feature_selection import SelectKBest
+
+from sklearn.feature_selection import f_regression
+
+# define number of features to keep
+
+k = 10
+
+# perform feature selection
+y = DATA['tg_numc_scope_1']
+selector = SelectKBest(f_regression, k=k).fit(numerical_features[~y.isna()], y[~y.isna()])
+X_new = selector.transform(numerical_features)
+# get feature names of selected features
+
+selected_features = numerical_features.columns[selector.get_support()]
+
+# print selected features
+print('SelectKBest elimination\n')
+print(f"features_select_k_best = {list(selected_features)}")
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(numerical_features[selected_features].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+
+# %%
+# %%
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from tqdm import tqdm
+# calculate VIF for each feature
+
+vif = pd.DataFrame()
+
+vif["VIF Factor"] = [variance_inflation_factor(numerical_features, i) for i in tqdm(range(numerical_features.shape[1]))]
+
+vif["features"] = numerical_features.columns
+
+# print VIF values
+# %%
+print('VIF elimination\n')
+print(f"features_VIF_under_10 = {vif[vif['VIF Factor'] < 10].features.tolist()}")
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(numerical_features[vif[vif["VIF Factor"] < 10].features.tolist()].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+# %%
+print('VIF elimination\n')
+print(f"features_VIF_under_5 = {vif[vif['VIF Factor'] < 5].features.tolist()}")
+# %%
+plt.figure(figsize=(25, 20))
+sns.heatmap(numerical_features[vif[vif["VIF Factor"] < 5].features.tolist()].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+# %%
+from sklearn.feature_selection import RFECV
+from xgboost import XGBRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+estimator = RandomForestRegressor()
+selector = RFECV(estimator, step=0.1, cv=10, verbose=True)
+selector = selector.fit(numerical_features[~y.isna()], y[~y.isna()])
+
+# %%
+
+plt.figure(figsize=(25, 20))
+sns.heatmap(numerical_features.iloc[:, selector.support_].corr().abs(), vmin=-1, vmax=1, cmap='bwr')
+
+# %%

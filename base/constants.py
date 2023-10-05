721d10fadc05884f51c82a9dc113486d751090b2
Olusanmi Hundogan <o.hundogan@gmail.com>
2023-10-05 13:10:24 +0200

    Adding categoricals back again

--- base/constants.py
+++ base/constants.py
@@ -22,167 +22,64 @@ IMPORTANT_EVALUATION_COLUMNS = [
     "train.MAPE",
 ]
 
+FEATURE_SET_CATEGORICALS = [
+    'ft_catm_country_code',
+    'ft_catm_industry_name',
+    'ft_catm_sector_name',
+]
+
 # ===== BELOW ARE FEATURE SETS IDENTIFIED USING SIMPLE METHODS =====
 # The analysis can be found in notebooks/analyze_feature_set_initial_discovery.py
 
-# This is the set of features used before the 12data integration 
+# This is the set of features used before the 12data integration
 FEATURE_SET_OLD = [
-    'ft_numc_cash',
-    'ft_numd_employees', 
-    'ft_numc_equity',
-    'ft_numc_inventories', 
-    'ft_numc_market_cap', 
-    'ft_numc_net_income', 
-    'ft_numc_ppe',
-    'ft_numc_rd_expenses', 
-    'ft_numc_revenue',
-    'ft_numc_roa',
-    'ft_numc_roe',
-    'ft_numc_stock_return',
-    'ft_numc_total_assets',
-    'ft_numc_total_liabilities'
-]
+    'ft_numc_cash', 'ft_numd_employees', 'ft_numc_equity', 'ft_numc_inventories', 'ft_numc_market_cap', 'ft_numc_net_income', 'ft_numc_ppe', 'ft_numc_rd_expenses',
+    'ft_numc_revenue', 'ft_numc_roa', 'ft_numc_roe', 'ft_numc_stock_return', 'ft_numc_total_assets', 'ft_numc_total_liabilities'
+] + FEATURE_SET_CATEGORICALS
 
 # This set is a reduction in which features where eliminated iteratively when they had a high correlation (>=0.5) with another feature
 FEATURE_SET_CORR_ELIMINATION = [
-    'ft_numc_acrued_expenses',
-    'ft_numc_common_stock_dividends',
-    'ft_numc_deferred_liabilities',
-    'ft_numc_diluted_shares_outstanding',
-    'ft_numc_eps_diluted',
-    'ft_numc_financial_assets',
-    'ft_numc_financing_activities.common_stock_issuance',
-    'ft_numc_financing_activities.common_stock_repurchase',
-    'ft_numc_financing_activities.short_term_debt_issuance',
-    'ft_numc_goodwill',
-    'ft_numc_hedging_assets',
-    'ft_numc_interest_paid',
-    'ft_numc_investing_activities_purchase_of_investments',
-    'ft_numc_investing_actvities.net_acquisitions',
-    'ft_numc_investing_actvities.net_intangibles',
-    'ft_numc_minority_interests',
-    'ft_numc_operating_activities.accounts_payable',
-    'ft_numc_operating_activities.other_assets_liabilities',
-    'ft_numc_operating_activities.other_non_cash_items',
-    'ft_numc_other_current_assets',
-    'ft_numc_other_financing_charges',
-    'ft_numc_other_income_expense',
-    'ft_numc_other_non_current_assets',
-    'ft_numc_other_non_current_liabilities',
-    'ft_numc_other_shareholders_equity',
-    'ft_numc_preferred_stock_dividends',
-    'ft_numc_prior_tg_numc_scope_1',
-    'ft_numc_prior_tg_numc_scope_2',
-    'ft_numc_prior_tg_numc_scope_3',
-    'ft_numc_restricted_cash',
-    'ft_numc_roa',
-    'ft_numc_roe',
-    'ft_numc_tax_payable',
-    'ft_numc_tresury_stock'
-]
+    'ft_numc_acrued_expenses', 'ft_numc_common_stock_dividends', 'ft_numc_deferred_liabilities', 'ft_numc_diluted_shares_outstanding', 'ft_numc_eps_diluted',
+    'ft_numc_financial_assets', 'ft_numc_financing_activities.common_stock_issuance', 'ft_numc_financing_activities.common_stock_repurchase',
+    'ft_numc_financing_activities.short_term_debt_issuance', 'ft_numc_goodwill', 'ft_numc_hedging_assets', 'ft_numc_interest_paid',
+    'ft_numc_investing_activities_purchase_of_investments', 'ft_numc_investing_actvities.net_acquisitions', 'ft_numc_investing_actvities.net_intangibles',
+    'ft_numc_minority_interests', 'ft_numc_operating_activities.accounts_payable', 'ft_numc_operating_activities.other_assets_liabilities',
+    'ft_numc_operating_activities.other_non_cash_items', 'ft_numc_other_current_assets', 'ft_numc_other_financing_charges', 'ft_numc_other_income_expense',
+    'ft_numc_other_non_current_assets', 'ft_numc_other_non_current_liabilities', 'ft_numc_other_shareholders_equity', 'ft_numc_preferred_stock_dividends',
+    'ft_numc_prior_tg_numc_scope_1', 'ft_numc_prior_tg_numc_scope_2', 'ft_numc_prior_tg_numc_scope_3', 'ft_numc_restricted_cash', 'ft_numc_roa', 'ft_numc_roe',
+    'ft_numc_tax_payable', 'ft_numc_tresury_stock'
+] + FEATURE_SET_CATEGORICALS
 
 # This set is a reduction in which features where eliminated WITHOUT iterative removals when they had a high correlation (>=0.5) with another feature
 FEATURE_SET_STRICT_CORR_ELIMINATION = [
-    'ft_numc_hedging_assets',
-    'ft_numc_investing_actvities.net_acquisitions',
-    'ft_numc_other_financing_charges',
-    'ft_numc_prior_tg_numc_scope_1',
-    'ft_numc_prior_tg_numc_scope_2',
-    'ft_numc_prior_tg_numc_scope_3',
-    'ft_numc_roa',
-    'ft_numc_roe'
-]
-
+    'ft_numc_hedging_assets', 'ft_numc_investing_actvities.net_acquisitions', 'ft_numc_other_financing_charges', 'ft_numc_prior_tg_numc_scope_1', 'ft_numc_prior_tg_numc_scope_2',
+    'ft_numc_prior_tg_numc_scope_3', 'ft_numc_roa', 'ft_numc_roe'
+] + FEATURE_SET_CATEGORICALS
 
 # This set is a reduction in which the select K-Best method was used. (sklearn implementation)
 FEATURE_SET_SELECT_K_BEST = [
-    'ft_numc_basic_shares_outstanding', 
-    'ft_numc_deferred_liabilities',
-    'ft_numc_diluted_shares_outstanding', 
-    'ft_numc_hedging_assets', 
-    'ft_numc_minority_interest', 
-    'ft_numc_operating_activities.depreciation', 
-    'ft_numc_prior_tg_numc_scope_1', 
-    'ft_numc_prior_tg_numc_scope_2', 
-    'ft_numc_prior_tg_numc_scope_3', 
-    'ft_numc_tax_payable'
-]
-
+    'ft_numc_basic_shares_outstanding', 'ft_numc_deferred_liabilities', 'ft_numc_diluted_shares_outstanding', 'ft_numc_hedging_assets', 'ft_numc_minority_interest',
+    'ft_numc_operating_activities.depreciation', 'ft_numc_prior_tg_numc_scope_1', 'ft_numc_prior_tg_numc_scope_2', 'ft_numc_prior_tg_numc_scope_3', 'ft_numc_tax_payable'
+] + FEATURE_SET_CATEGORICALS
 
 # This set is a reduction in which Variance Inflation Factor (VIF) technique was used. Removed everything with VIV > 10
 FEATURE_SET_VIF_UNDER_10 = [
-    'ft_numc_acrued_expenses', 
-    'ft_numc_additional_paid_in_capital', 
-    'ft_numc_assets_held_for_sale', 
-    'ft_numc_cash', 
-    'ft_numc_cash_equivalents', 
-    'ft_numc_derivative_product_liabilities',
-    'ft_numc_financial_assets', 
-    'ft_numc_free_cash_flow', 
-    'ft_numc_goodwill', 
-    'ft_numc_hedging_assets', 
-    'ft_numc_income', 
-    'ft_numc_income_tax_paid', 
-    'ft_numc_intangible_assets', 
-    'ft_numc_interest_paid', 
-    'ft_numc_investing_actvities.net_intangibles', 
-    'ft_numc_investment_properties', 
-    'ft_numc_investments_and_advances', 
-    'ft_numc_leases', 
-    'ft_numc_long_term_provisions', 
-    'ft_numc_minority_interest', 
-    'ft_numc_minority_interests', 
-    'ft_numc_other_current_assets', 
-    'ft_numc_other_income_expense', 
-    'ft_numc_other_non_current_assets', 
-    'ft_numc_other_operating_expenses', 
-    'ft_numc_other_receivables', 
-    'ft_numc_other_shareholders_equity', 
-    'ft_numc_other_short_term_investments', 
-    'ft_numc_pensions', 
-    'ft_numc_preferred_stock_dividends', 
-    'ft_numc_prepaid_assets', 
-    'ft_numc_prior_tg_numc_scope_1', 
-    'ft_numc_prior_tg_numc_scope_2', 
-    'ft_numc_prior_tg_numc_scope_3', 
-    'ft_numc_rd_expenses', 
-    'ft_numc_restricted_cash',
-    'ft_numc_roa', 
-    'ft_numc_roe', 
-    'ft_numc_total_assets', 
+    'ft_numc_acrued_expenses', 'ft_numc_additional_paid_in_capital', 'ft_numc_assets_held_for_sale', 'ft_numc_cash', 'ft_numc_cash_equivalents',
+    'ft_numc_derivative_product_liabilities', 'ft_numc_financial_assets', 'ft_numc_free_cash_flow', 'ft_numc_goodwill', 'ft_numc_hedging_assets', 'ft_numc_income',
+    'ft_numc_income_tax_paid', 'ft_numc_intangible_assets', 'ft_numc_interest_paid', 'ft_numc_investing_actvities.net_intangibles', 'ft_numc_investment_properties',
+    'ft_numc_investments_and_advances', 'ft_numc_leases', 'ft_numc_long_term_provisions', 'ft_numc_minority_interest', 'ft_numc_minority_interests', 'ft_numc_other_current_assets',
+    'ft_numc_other_income_expense', 'ft_numc_other_non_current_assets', 'ft_numc_other_operating_expenses', 'ft_numc_other_receivables', 'ft_numc_other_shareholders_equity',
+    'ft_numc_other_short_term_investments', 'ft_numc_pensions', 'ft_numc_preferred_stock_dividends', 'ft_numc_prepaid_assets', 'ft_numc_prior_tg_numc_scope_1',
+    'ft_numc_prior_tg_numc_scope_2', 'ft_numc_prior_tg_numc_scope_3', 'ft_numc_rd_expenses', 'ft_numc_restricted_cash', 'ft_numc_roa', 'ft_numc_roe', 'ft_numc_total_assets',
     'ft_numc_tresury_stock'
-]
+] + FEATURE_SET_CATEGORICALS
 
 # This set is a reduction in which Variance Inflation Factor (VIF) technique was used. Removed everything with VIV > 5
 FEATURE_SET_VIF_UNDER_5 = [
-    'ft_numc_acrued_expenses', 
-    'ft_numc_additional_paid_in_capital', 
-    'ft_numc_assets_held_for_sale', 
-    'ft_numc_cash_equivalents', 
-    'ft_numc_derivative_product_liabilities', 
-    'ft_numc_financial_assets', 
-    'ft_numc_goodwill', 
-    'ft_numc_hedging_assets', 
-    'ft_numc_income_tax_paid', 
-    'ft_numc_interest_paid', 
-    'ft_numc_investing_actvities.net_intangibles', 
-    'ft_numc_investment_properties', 
-    'ft_numc_leases', 
-    'ft_numc_long_term_provisions', 
-    'ft_numc_minority_interests', 
-    'ft_numc_other_current_assets', 
-    'ft_numc_other_income_expense', 
-    'ft_numc_other_non_current_assets', 
-    'ft_numc_other_operating_expenses', 
-    'ft_numc_other_shareholders_equity', 
-    'ft_numc_other_short_term_investments', 
-    'ft_numc_preferred_stock_dividends', 
-    'ft_numc_prepaid_assets', 
-    'ft_numc_prior_tg_numc_scope_1', 
-    'ft_numc_prior_tg_numc_scope_2', 
-    'ft_numc_prior_tg_numc_scope_3', 
-    'ft_numc_rd_expenses', 
-    'ft_numc_roa', 
-    'ft_numc_roe', 
-    'ft_numc_tresury_stock'
-]
+    'ft_numc_acrued_expenses', 'ft_numc_additional_paid_in_capital', 'ft_numc_assets_held_for_sale', 'ft_numc_cash_equivalents', 'ft_numc_derivative_product_liabilities',
+    'ft_numc_financial_assets', 'ft_numc_goodwill', 'ft_numc_hedging_assets', 'ft_numc_income_tax_paid', 'ft_numc_interest_paid', 'ft_numc_investing_actvities.net_intangibles',
+    'ft_numc_investment_properties', 'ft_numc_leases', 'ft_numc_long_term_provisions', 'ft_numc_minority_interests', 'ft_numc_other_current_assets', 'ft_numc_other_income_expense',
+    'ft_numc_other_non_current_assets', 'ft_numc_other_operating_expenses', 'ft_numc_other_shareholders_equity', 'ft_numc_other_short_term_investments',
+    'ft_numc_preferred_stock_dividends', 'ft_numc_prepaid_assets', 'ft_numc_prior_tg_numc_scope_1', 'ft_numc_prior_tg_numc_scope_2', 'ft_numc_prior_tg_numc_scope_3',
+    'ft_numc_rd_expenses', 'ft_numc_roa', 'ft_numc_roe', 'ft_numc_tresury_stock'
+] + FEATURE_SET_CATEGORICALS

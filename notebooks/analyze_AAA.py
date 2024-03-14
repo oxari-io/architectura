f2c90d5042a2b39c1a23885ea4793485147715e6
experiment-server <experiment-server@digitalocean.com>
2024-03-14 09:32:29 +0000

    Impl: Finished AAA results

--- notebooks/analyze_AAA.py
+++ notebooks/analyze_AAA.py
@@ -14,8 +14,13 @@ df_results["scope_estimator"] = pd.Categorical(df_results["scope_estimator"])
 df_results = df_results[~df_results["feature_selector"].str.startswith("PCA")]
 df_results
 # %%
-list(df_results.columns.values)
-# %%
 sns.boxplot(data=df_results[df_results!="DummyEstimator"], x="raw.sMAPE", y="scope_estimator")
 # %%
 sns.histplot(data=df_results[df_results!="DummyEstimator"], x="raw.sMAPE", hue="scope_estimator", kde=True)
+# %%
+res = df_results.sort_values("raw.sMAPE")[['imputer', 'fin_transformer',
+       'cat_transformer', 'scope_transformer', 'preprocessor',
+       'feature_selector', 'scope_estimator']+df_results.filter(regex="^(raw|train|test)").columns.tolist()].head(50)#.iloc[0].head(50)
+# %%
+res.iloc[0][["test.sMAPE", "train.sMAPE", "raw.sMAPE"]]
+# %%
